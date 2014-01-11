513a2bca5a7ba474ece268088c65ebe1f6e955d0
Aszalós Gábor <elmisto@gmail.com>
2014-01-11 15:43:52 +0100

    Change toolbar element to tabs

--- js/nav.js
+++ js/nav.js
@@ -1,48 +1,35 @@
-// Incompatibility warning -----------------------------------------------------
+// WebRTC warning --------------------------------------------------------------
 if(navigator.getMedia === undefined) {
   document.querySelector('#confirm').className = 'fade-in';
 }
-document.getElementById('btn-confirm').addEventListener ('click', function () {
-  document.querySelector('#confirm').className = 'fade-out';
-});
 
 // Settings open ---------------------------------------------------------------
 document.querySelector('#btn-settings').addEventListener ('click', function () {
   cancelAnimFrame(requestId);
-  document.querySelector('#settings').className = 'current';
-  document.querySelector('[data-position="current"]').className = 'left';
-  var animend = (/webkit/i).test(navigator.appVersion) ? 'webkitAnimationEnd' : 'animationend';
 
-  document.addEventListener(animend, function animationend() {
-    document.removeEventListener(animend, animationend);
-    
-    document.getElementById("a-frequency").setAttribute(
-      'aria-valuenow',
-      settings.baseA4Frequency
-    );
-    
-    document.getElementById("a-frequency").onchange = function(e) {
-      document.querySelector("#a-frequency label").textContent = Math.round(e.detail / 10) * 10 +  " Hz";
-    };
-    
-    document.getElementById("microphone-gain").setAttribute(
-      'aria-valuenow',
-      settings.microphoneGain * 10
-    );
-    
-    document.getElementById("microphone-gain").onchange = function(e) {
-      document.querySelector("#microphone-gain label").textContent = Math.round(e.detail / 5) * 5 / 10;
-    };
+  document.getElementById("a-frequency").setAttribute(
+    'aria-valuenow',
+    settings.baseA4Frequency
+  );
+  
+  document.getElementById("a-frequency").onchange = function(e) {
+    document.querySelector("#a-frequency label").textContent = Math.round(e.detail / 10) * 10 +  " Hz";
+  };
+  
+  document.getElementById("microphone-gain").setAttribute(
+    'aria-valuenow',
+    settings.microphoneGain * 10
+  );
+  
+  document.getElementById("microphone-gain").onchange = function(e) {
+    document.querySelector("#microphone-gain label").textContent = Math.round(e.detail / 5) * 5 / 10;
+  };
 
-    utils.seekbars.init();
-  });
+  utils.seekbars.init();
 });
 
 // Settings apply --------------------------------------------------------------
-document.querySelector('#btn-settings-back').addEventListener ('click', function () {
-  document.querySelector('#settings').className = 'right';
-  document.querySelector('[data-position="current"]').className = 'current';
-  
+document.querySelector('#btn-tuner').addEventListener ('click', function () {  
   settings.baseA4Frequency = Math.round(document.getElementById('a-frequency').getAttribute('aria-valuenow') / 10) * 10;
   settings.baseC2Frequency = Math.pow(2, -33 / 12) * settings.baseA4Frequency;
   settings.microphoneGain  = Math.round(document.getElementById('microphone-gain').getAttribute('aria-valuenow') / 5) * 5 / 10;
@@ -51,5 +38,4 @@ document.querySelector('#btn-settings-back').addEventListener ('click', function
   microphone.setGain(settings.microphoneGain);
   requestId = requestAnimFrame(animate);
 });
-
 
